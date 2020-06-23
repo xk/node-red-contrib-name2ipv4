e5aac08e3677fb2faa973021f9fb8d21c9af8587
xk <jorge@jorgechamorro.com>
2020-06-23 16:00:17 +0200

    2nd commit

--- name2ipv4.js
+++ name2ipv4.js
@@ -1,3 +1,5 @@
+//2020-07-23 name2ipv4 jorge@jorgechamorro.com
+
 module.exports= function(RED) {
 
     "use strict";
@@ -18,28 +20,28 @@ module.exports= function(RED) {
               }
             };
 
-						Si msg.payload contiene domain_name es usa msg.payload.domain_name
-						Si no, se intenta usar config.domain_name
+            //Si msg.payload contiene domain_name es usa msg.payload.domain_name
+            //Si no, se intenta usar config.domain_name
 
-						var err;
-						var domain;
-						if (msg.payload.domain_name) {
-							if (typeof msg.payload.domain_name === "string") {
-								domain= msg.payload.domain_name;
-							}
-							else err= "msg.payload.domain_name is not a string";
-						}
-						else if (config.domain_name) {
-							if (typeof config.domain_name === "string") {
-								domain= config.domain_name;
-							}
-							else err= "config.domain_name is not a string";
-						}
-						else err= "invalid domain name";
+            var err;
+            var domain;
+            if (msg.payload.domain_name) {
+              if (typeof msg.payload.domain_name === "string") {
+                domain= msg.payload.domain_name;
+              }
+              else err= "msg.payload.domain_name is not a string";
+            }
+            else if (config.domain_name) {
+              if (typeof config.domain_name === "string") {
+                domain= config.domain_name;
+              }
+              else err= "config.domain_name is not a string";
+            }
+            else err= "invalid domain name";
 
-						if (err) {
+            if (err) {
 
-						}
+            }
             else dns.resolve4(domain, cb);
 
             return msg;
